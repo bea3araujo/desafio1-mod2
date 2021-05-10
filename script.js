7a662af102b6f88b009f638672b6c0e8229ed8d9
bea3araujo <beatrizaraujo2507@gmail.com>
2021-05-10 19:54:31 -0300

    fiz html, estilizei o html e fiz o JS - nele coloquei o variaveis e fiz 3 console.log para colocar meu nome, idade e a soma de dois valores

---
+++ script.js
@@ -0,0 +1,22 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+let name = "Beatriz"
+let age = 23
+let year = 2021
+let souhumano = true
+let number = 3
+let _number = 2
+
+console.log("Sou a", name)
+console.log("Meu ano de nascimento é", year-age)
+console.log( number + _number)
